6223d5ee6621ce4e49dfc34f001002c408738ff5
grimberg12 <grimbergnicolai@gmail.com>
2026-04-13 13:28:55 +0200

    tilføjet 2 filer til ejendomsinfo

--- Frontend/js/ejendom.js
+++ Frontend/js/ejendom.js
@@ -73,3 +73,63 @@ document.addEventListener("DOMContentLoaded", () => {
   console.log("Fundet ejendom:", property);
 
 });
+
+const rentalSelect = document.getElementById("rental");
+const rentalInfo = document.getElementById("rentalInfo");
+
+function toggleRentalInfo() {
+    const value = rentalSelect.value; // altid string fra <select>
+
+    if (value === "true") {
+        rentalInfo.style.display = "block";
+    } else {
+        rentalInfo.style.display = "none";
+    }
+}
+
+// Kør ved ændring
+rentalSelect.addEventListener("change", toggleRentalInfo);
+
+// Kør ved load (så den er korrekt fra start)
+toggleRentalInfo();
+
+//Forslå mortgage ud fra equity og purchase price.
+const purchasePriceInput = document.getElementById("purchasePrice");
+const equityInput = document.getElementById("equity");
+const mortgageInput = document.getElementById("mortgage");
+
+function updateMortgagePlaceholder() {
+    const purchasePrice = parseFloat(purchasePriceInput.value);
+    const equity = parseFloat(equityInput.value);
+
+    // Valider input
+    if (!isNaN(purchasePrice) && !isNaN(equity)) {
+        const result = purchasePrice - equity;
+
+        if (result >= 0) {
+            mortgageInput.placeholder = result.toLocaleString("da-DK") + " kr. (foreslået lån)";
+        } else {
+            mortgageInput.placeholder = "Realkredit lån";
+        }
+    } else {
+        mortgageInput.placeholder = "Realkredit lån";
+    }
+}
+
+// Lyt på begge inputs
+purchasePriceInput.addEventListener("input", updateMortgagePlaceholder);
+equityInput.addEventListener("input", updateMortgagePlaceholder);
+
+//when opret investeringscase is clicked open form (defeault is hidden)
+const createCaseBtn = document.getElementById("openFormButton-investeringscase");
+const inputSection = document.querySelector(".input-section-investeringscase");
+
+createCaseBtn.addEventListener("click", () => {
+    if (inputSection.style.display === "none" || inputSection.style.display === "") {
+        inputSection.style.display = "block";
+        createCaseBtn.textContent = "Luk form";
+    } else {
+        inputSection.style.display = "none";
+        createCaseBtn.textContent = "Opret ny investeringscase";
+    }
+});
